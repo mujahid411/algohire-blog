659f6f0da346ecc096213b2ffaad3b62347027eb
syedmujahidahmed <syedmujahidahmed0@gmail.com>
2024-02-04 22:43:18 +0530

    changed vercel json

--- server/app.js
+++ server/app.js
@@ -12,20 +12,20 @@ import cors from 'cors'
 dbConnect();
 const app = express();
 
-// app.use(cors(
-    //     {
-//         origin: ["http://localhost:3000/"],
-//         methods: ["POST", "GET"],
-//     }
-// ));
+app.use(cors(
+        {
+        origin: "*",
+        methods: ["POST", "GET","PUT","DELETE"],
+    }
+));
 // app.use((req, res, next) => {
 //     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
 //     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 //     next();
 //   });
-app.use(cors({
-    origin: 'http://localhost:3000' 
-}));
+// app.use(cors({
+//     origin: 'http://localhost:3000' 
+// }));
 
 const port = 5050;
 
