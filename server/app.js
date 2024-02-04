d22b744221e4093744800f30df2c95327461f42a
syedmujahidahmed <syedmujahidahmed0@gmail.com>
2024-02-04 23:42:16 +0530

    access control headers

--- server/app.js
+++ server/app.js
@@ -11,6 +11,12 @@ import cors from 'cors'
 
 dbConnect();
 const app = express();
+app.use((_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+  
+    next();
+  });
 
 app.use(cors(
         {
