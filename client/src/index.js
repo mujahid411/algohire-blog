556693da4fbbcf9ea3ba810cd18999f686221331
syedmujahidahmed <syedmujahidahmed0@gmail.com>
2024-02-06 14:54:08 +0530

    env removed

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import AppContext from './Context';
-import dotenv from 'dotenv';
 
-dotenv.config(); 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
