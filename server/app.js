254f32bbaa94fa0edce4b8fc4ab7c0e10f946db5
syedmujahidahmed <syedmujahidahmed0@gmail.com>
2024-02-06 21:01:36 +0530

    removing unwanted code

--- server/app.js
+++ server/app.js
@@ -12,52 +12,10 @@ dbConnect();
 const app = express();
 
 app.use(cors({
-    origin: 'https://algohire-blog-client.vercel.app', // Adjust this to match your frontend's origin
+    origin: 'https://algohire-blog-client.vercel.app', 
     credentials: true,
 }));
 
-// app.options('*', cors()) // Enable preflight requests for all routes
-
-
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     next();
-//   });
-
-// app.use(cors(
-//         {
-//         origin: "https://algohire-blog-client.vercel.app",
-//         methods: ["POST", "GET","PUT","DELETE"],
-//         credentials: true,
-//     }
-// ));
-// app.use(cors({
-//     origin: function(origin, callback){
-//         // Define array of allowed origins
-//         const allowedOrigins = ['http://localhost:3000', 'https://algohire-blog-client.vercel.app'];
-
-//         // Allow requests with no origin 
-//         // (like mobile apps or curl requests)
-//         if(!origin) return callback(null, true);
-//         if(allowedOrigins.indexOf(origin) === -1){
-//             var msg = 'The CORS policy for this site does not ' +
-//                       'allow access from the specified Origin.';
-//             return callback(new Error(msg), false);
-//         }
-//         return callback(null, true);
-//     },
-//             methods: ["POST", "GET","PUT","DELETE"],
-
-// }));
-
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     next();
-//   });
-// app.use(cors({
-//     origin: 'http://localhost:3000' 
-// }));
 
 const port = 5050;
 
@@ -71,15 +29,6 @@ app.get('/', (req, res) => {
 })
 
 
-
-app.get('/allBlogs', async (req, res) => {
-    try {
-        console.log('allblogs')
-        return res.status(200).json({ success: 'blog created successfully!' })
-    } catch (error) {
-        console.error(error)
-    }
-})
 
 
 app.use('/api/user', userRoutes)
